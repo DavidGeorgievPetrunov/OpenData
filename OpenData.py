40dec731ede785d0268bebc85f4ca96b3e176700
unknown <dpetrunov@cifpfbmoll.eu>
2023-11-23 23:02:07 +0100

    first Commit

---
+++ OpenData.py
@@ -0,0 +1,173 @@
+from pathlib import Path
+from pyexpat import features
+import statistics
+from fastapi import FastAPI, status, HTTPException, Request
+from fastapi.responses import FileResponse, JSONResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from fastapi import HTTPException
+import requests
+import csv
+from fastapi.middleware.cors import CORSMiddleware
+from io import StringIO
+
+import uvicorn
+
+app = FastAPI()
+
+origins = [""]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
+
+API_URL = "https://services1.arcgis.com/nCKYwcSONQTkPA4K/arcgis/rest/services/TurismoPaisResid/FeatureServer/0/query"
+static_dir = Path('./static')
+
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+
+class Comunidades(BaseModel):
+    comunidad: str
+
+
+class Pais(BaseModel):
+    comunidad: str
+    pais: str
+
+
+class Area(BaseModel):
+    tamañoMin: int
+    tamañoMax: int
+    comunidades: str
+
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/static/index.html", status_code=status.HTTP_302_FOUND)
+
+
+@app.get("/autobuses")
+async def autobuses():
+    params = {
+        "outFields": "*",
+        "where": "1=1",
+        "f": "geojson",
+    }
+
+    response = requests.get(API_URL, params=params)
+    data = response.json()
+
+    features = data["features"]
+
+    return features
+
+
+@app.post("/autobuses/aaaa")
+async def autobuses(comunidades: Comunidades):
+    comunidad = comunidades.comunidad
+
+    print(comunidad)
+
+    params = {
+        "outFields": "*",
+        "where": f"Texto = '{comunidad}'",
+        "outSR": "4326",
+        "f": "json",
+    }
+
+    response = requests.get(API_URL, params=params)
+    data = response.json()
+
+    features = data['features'][0]['attributes']
+
+    return features
+
+
+@app.post("/autobuses/bbbb")
+async def autobuses(pais: Pais):
+    paisElegido = pais.pais
+    comunidad = pais.comunidad
+    pais = pais.pais
+    valorTotal = 0
+    valorPais = 0
+
+    print(comunidad)
+    print(pais)
+
+    params = {
+        "outFields": "*",
+        "where": f"Texto = '{comunidad}'",
+        "outSR": "4326",
+        "f": "json",
+    }
+
+    response = requests.get(API_URL, params=params)
+    data = response.json()
+
+    features = data['features'][0]['attributes']
+
+    valorTotal = features["Total"]
+    valorPais = features[f'{paisElegido}']
+
+    return (valorPais/valorTotal) * 100
+
+
+@app.post("/autobuses/cccc")
+async def autobuses(comunidades: Comunidades):
+    comunidad = comunidades.comunidad
+    area = 0
+
+    print(comunidad)
+
+    params = {
+        "outFields": "*",
+        "where": f"Texto = '{comunidad}'",
+        "outSR": "4326",
+        "f": "json",
+    }
+
+    response = requests.get(API_URL, params=params)
+    data = response.json()
+
+    features = data['features'][0]['attributes']
+    area = features['Shape__Area']
+
+    return area
+
+
+@app.post("/autobuses/dddd")
+async def autobuses(comunidades: Comunidades):
+    comunidad = comunidades.comunidad
+    poblacionEspañola = 0
+    poblacionTotal = 0
+
+    print(comunidad)
+
+    params = {
+        "outFields": "*",
+        "where": f"Texto = '{comunidad}'",
+        "outSR": "4326",
+        "f": "json",
+    }
+
+    response = requests.get(API_URL, params=params)
+    data = response.json()
+
+    features = data['features'][0]['attributes']
+
+    poblacionTotal = features["Total"]
+    poblacionEspañola = features["Residentes_en_España"]
+
+    return poblacionTotal-poblacionEspañola
+
+if __name__ == "__main__":
+    print("-> Inicio integrado de servicIo web")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+else:
+    print("=> Iniciado desde el servidor web")
+    print("   Módulo python iniciado:", __name__)
---
+++ static/script.js
@@ -0,0 +1,114 @@
+async function mostrarDatos() {
+    const myApiUrl = "http://213.94.41.90:8000/autobuses";
+    var elemento = document.getElementById("ResultadoMostrarDatos");
+    var select = document.getElementById("comunidades").value;
+
+    elemento.innerHTML="Calculando"
+
+    const json = {
+        comunidad: select,
+    }
+
+    console.log("Comunidad Autónoma seleccionada:", select);
+
+    const response = await fetch(myApiUrl+"/aaaa",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(json),
+    });
+
+    const data = await response.json();
+    console.log(data)
+
+    elemento.innerHTML=JSON.stringify(data);
+}
+
+async function compararDatosTotal(){
+    const myApiUrl = "http://213.94.41.90:8000/autobuses";
+
+    var elemento = document.getElementById("ResultadoCompararTotalyPais") 
+    var select = document.getElementById("comunidades").value;
+    var select2 = document.getElementById("pais").value
+
+    elemento.innerHTML = "Calculando"
+
+    const json = {
+        comunidad: select,
+        pais: select2,
+    }
+
+    console.log("Comunidad Autónoma y pais seleccionados:", select, select2);
+
+    const response = await fetch(myApiUrl+"/bbbb",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(json),
+    });
+
+    const data = await response.json();
+    console.log(data)
+    console.log(elemento);
+    elemento.innerHTML = data;
+
+}
+
+async function mostrarArea(){
+
+    const myApiUrl = "http://213.94.41.90:8000/autobuses";
+
+    var elemento = document.getElementById("ResultadosAreaPais") 
+    var select = document.getElementById("comunidades").value;
+
+    elemento.innerHTML = "Calculando"
+
+    console.log(select)
+
+    const json = {
+        comunidad: select,
+    }
+
+    const response = await fetch(myApiUrl+"/cccc",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(json),
+    });
+
+    const data = await response.json();
+    console.log(data)
+    elemento.innerHTML = JSON.stringify(data);
+}
+
+async function mostrarPoblacionE(){
+    const myApiUrl = "http://213.94.41.90:8000/autobuses";
+    var elemento = document.getElementById("ResultadosAreaPaisMayor");
+    var select = document.getElementById("comunidades").value;
+
+    elemento.innerHTML="Calculando"
+
+    console.log(select)
+
+    const json = {
+        comunidad: select,
+    }
+
+    console.log("Comunidad Autónoma seleccionada:", select);
+
+    const response = await fetch(myApiUrl+"/dddd",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(json),
+    });
+
+    const data = await response.json();
+    console.log(data)
+
+    elemento.innerHTML=JSON.stringify(data);
+}
